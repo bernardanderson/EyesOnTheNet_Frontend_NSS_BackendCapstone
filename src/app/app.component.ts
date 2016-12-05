9185cda79a7ea5378aae61de2fe60fa4318db696
Bernard Anderson <bernardanderson@gmail.com>
2016-12-05 14:01:08 -0600

    npm'd semantic-ui and jquery Added the appropriate links in the angular-cli.json for the added jQuery and semantic-ui js/css files. (So no html <link> <script> needed)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works and is even better!';
+  title: string = 'app works and is even better!';
+  
+  constructor(private http: Http) {}
+
+  camImage: string = "";
+
 }
