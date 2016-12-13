b7ca5ef8ec181d02d8357c39eea83c76f221acff
bernardanderson <bernard.anderson@gmail.com>
2016-12-12 21:24:18 -0600

    Added Camera View, timer, and styling

--- src/app/cameraDisplay.component.ts
+++ src/app/cameraDisplay.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnDestroy } from '@angular/core'; // Add Input, OnDestroy for menu communication
+import { Component, Input, OnInit, OnDestroy } from '@angular/core'; // Add Input, OnDestroy for menu communication
 import { Http, Headers, Response} from '@angular/http';
 import { Router } from '@angular/router';
 import { MenuService } from './menu.service';           // Needed for menu communication
 import { Subscription }   from 'rxjs/Subscription';     // Needed for menu communication
+import { Observable } from 'rxjs/Observable';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable'; // For Timer
 import 'rxjs/add/operator/map';
 //
 import { GlobalVariables } from './global';
@@ -16,14 +18,21 @@ declare var $: any;
 })
 
 // Add implements OnDestroy for menu communictation
-export class CameraDisplayComponent implements OnDestroy {  
+
+export class CameraDisplayComponent implements OnInit, OnDestroy {  
 
     // Inputted from menu communictation in App.Component
     @Input() selectedMenu:string;                           
     
     ngOnInit() {
         this.menuService.activateMenu(true);
-        //this.getCameraList();
+        this.getCameraList();
+        
+        IntervalObservable.create(5000).subscribe(timeKeeper => {
+
+            console.log("Hello");             
+        
+        });
     }
 
     // Prevents memory leakage when this view is destroyed
@@ -32,33 +41,58 @@ export class CameraDisplayComponent implements OnDestroy {
     }
 
     // Needed for menu communication
-    subscription:Subscription;                              
-    
-    userCameraList: {
-        id: string,
+    subscription:Subscription;
+
+    userCameraList: {}[] = [];
+    viewingCameras: {
+        cameraHashId: number,
+        cameraURL: string,
         name: string,
         location: string
-    }[];
+    } [] = [];
 
     // The formatted constructor receives the menu information when changed in parent
-    constructor(private menuService: MenuService) {          
+    constructor(private menuService: MenuService, private http: Http) {  
         this.subscription = menuService.selectedMenuItem$.subscribe(
         menuItem => {
-                         // Put code here to run functions or access the menuItems
+        // Put code here to run functions or access the menuItems
         });
     }
 
+    // Gets the list of user cameras and populates the Camera Cards
+    getCameraList() {
+      this.http.get(`http://${GlobalVariables.serverIP}/api/camera`,
+      { withCredentials: true } )
+      .map(res => this.extractData(res))
+      .subscribe(
+        data => {
+          this.userCameraList = this.userCameraList.concat(data);
+        },
+        err => {
+          console.log(err);          
+        })
+    }
 
+    private extractData(res: Response) {
+      let body;
+      // check if empty, before call json
+      if (res.text()) {
+          body = res.json();
+      }
+      return body || {};
+    }
 
+    addCamerasToView(sentCamera): boolean {
 
-
-
-
-
-
-
-
-
-
-
+        sentCamera.cameraURL =`http://${GlobalVariables.serverIP}/api/camera/${sentCamera.cameraIdHash}/snapshot?` + new Date().getTime();
+        
+        for (let i = 0; i < this.viewingCameras.length; i++) {
+            if (sentCamera.name === this.viewingCameras[i].name) {
+                this.viewingCameras[i] = sentCamera
+                return true;       
+            }
+        }
+        this.viewingCameras.push(sentCamera);
+        return true;
+    }
 }
